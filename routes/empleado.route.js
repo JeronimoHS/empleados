e316a7ea977a78dbe28cfe9a3fa66c38b88445e8
JeronimoHS <jeronimo.her.s@gmail.com>
2023-06-28 17:28:56 -0600

    version 1 de API

---
+++ routes/empleado.route.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const app = express()
+const empleadoRuta = express.Router()
+
+//declaramos un objeto del modelo 
+
+const Empleado = require('../models/Empleado')
+
+//agregar un nuevo empleado
+empleadoRuta.route('/create').post((req,res)=> {
+    Empleado.create(req.body)
+    .then((data) =>{
+        console.log('se inserto el doc')
+        res.send(data)
+    })
+    .catch((err)=>{
+        console.error(err)
+    })
+        
+})
+
+//obtenemos todos los empleados
+empleadoRuta.route('/empleados').get((req,res)=>{
+    Empleado.find()
+    .then((data)=>{
+        res.send(data)
+    })
+    .catch((err) =>{
+        console.error(err)
+    })
+})
+
+
+//obtener un solo empleado por id
+empleadoRuta.route('/empleado/:id').get((req,res)=>{
+    Empleado.findById(req.params.id)
+    .then((data) => {
+        res.send(data)
+    })
+    .catch((err)=>{
+        console.error(err)
+    })
+})
+
+//actualizar un empleado
+
+empleadoRuta.route('/update/:id').put((req,res) =>{
+    Empleado.findByIdAndUpdate(req.params.id, {
+        $set :req.body
+    })
+    .then((data)=>{
+        res.send(data)
+    })
+    .catch((err) =>{
+        console.error(err)
+    })
+})
+
+
+//eliminar empleado
+
+empleadoRuta.route('/delete/:id').delete((req,res,next) => {
+    Empleado.findByIdAndRemove(req.params.id)
+        .then((data)=> {
+            console.log('se elimino el doc')
+            res.send(data)
+        })
+        .catch((err)=>{
+            console.error(err)
+        })
+})
+
+module.exports =empleadoRuta;
